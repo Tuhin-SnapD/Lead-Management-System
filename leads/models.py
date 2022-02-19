e90ea46fd614850ce5d5e4c561ce1da34800e53d
Tuhin-SnapD <tuhinchakrabarty14@gmail.com>
2022-02-19 19:37:48 +0530

    registration form added

--- leads/models.py
+++ leads/models.py
@@ -1,3 +1,5 @@
 from django.db import models
 
-# Create your models here.
+class MyModel(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
